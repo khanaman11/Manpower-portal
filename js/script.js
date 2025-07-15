ef695ede29c5c6f125ca1aeaaef90b4cf1b12d8f
Aman khan <amanullahkhan11595@gmail.com>
2025-07-15 10:14:13 +0530

    last commit

--- js/script.js
+++ js/script.js
@@ -49,6 +49,7 @@ allOtpGroups.forEach(group => {
     });
 });
 
+// ************************ OTP input pr curser sift to next box script end ***************** //
 
 
 
@@ -76,6 +77,95 @@ function togglePassword(inputId, iconOrRef) {
 
 
 // //  ************************** password show and hide  script end *********************** //
+
+
+
+//  ******************* click to redirect login page to sign up page script start ************* // 
+
+
+function showLogin() {
+    const login = document.getElementById("loginSection");
+    const signup = document.getElementById("signupSection");
+    if (login && signup) {
+        signup.classList.remove("active");
+        login.classList.add("active");
+    }
+}
+
+function showSignup() {
+    const login = document.getElementById("loginSection");
+    const signup = document.getElementById("signupSection");
+    if (login && signup) {
+        login.classList.remove("active");
+        signup.classList.add("active");
+    }
+}
+
+//  ******************* click to redirect login page to sign up page script end ************* // 
+
+
+//  ********************** Enter new password script start ***************************** // 
+function showLoginBox() {
+    let login = document.getElementById("loginSection");
+    let newPassBox = document.getElementById("NewPassBox")
+
+    login.classList.add("active");
+    newPassBox.classList.remove("active")
+}
+
+//  ********************** Enter new password script end ***************************** // 
+
+
+// ************************* GO Back Page script start ******************************* // 
+function goBack(){
+    let backToPage = document.getElementById("backToPage");
+    let basicDetails = document.getElementById("basicDetails");
+    backToPage.classList.remove("active");
+    basicDetails.classList.add('active')
+}
+
+function goBack1(){
+    let uploadGoverment = document.getElementById("uploadGoverment");
+    let backToPage = document.getElementById("backToPage");
+    
+    uploadGoverment.classList.remove("active");
+    backToPage.classList.add("active")
+}
+
+
+function goBack2(){
+    let uploadGoverment = document.getElementById("uploadGoverment");
+    let educationPage = document.getElementById("educationPage")
+    educationPage.classList.remove("active")
+    uploadGoverment.classList.add("active");
+}
+
+
+function goBack3(){
+    let educationPage = document.getElementById("educationPage")
+    let ProfessionalExprience = document.getElementById("ProfessionalExprience");
+    ProfessionalExprience.classList.remove("active")
+    educationPage.classList.add("active")
+    
+}
+
+function goBack4(){
+    let bankDetails = document.getElementById("bankDetails");
+    let ProfessionalExprience = document.getElementById("ProfessionalExprience");
+    bankDetails.classList.remove("active");
+    ProfessionalExprience.classList.add("active")
+
+}
+
+// ************************* GO Back Page script end ******************************* // 
+
+
+
+
+
+
+
+
 
 
 
